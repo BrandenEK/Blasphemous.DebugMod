c27b35862645e0139674b8591330b35076dcce68
Branden Wood <brandenek03912@hotmail.com>
2023-04-01 18:03:41 -0400

    Restructure everything

--- DebugMod/Debugger.cs
+++ DebugMod/Debugger.cs
@@ -15,8 +15,6 @@ namespace DebugMod
         private const int NUM_TEXT_LINES = 4;
         private const float CAMERA_SPEED = 0.1f;
 
-        public bool DebugEnabled { get; private set; }
-
         private Sprite hitboxImage;
         private List<GameObject> sceneHitboxes = new List<GameObject>();
         private List<Text> textObjects = new List<Text>();
@@ -25,153 +23,67 @@ namespace DebugMod
         protected override void Initialize()
         {
             hitboxImage = FileUtil.loadDataImages("hitbox.png", 1, 1, 1, 0, true, out Sprite[] images) ? images[0] : null;
-            DebugEnabled = false;
             DisableFileLogging = true;
         }
 
         protected override void Update()
         {
-            if (Input.GetKeyDown(KeyCode.Equals))
+            if (Input.GetKeyDown(KeyCode.Keypad7))
             {
-                DebugEnabled = !DebugEnabled;
-                if (DebugEnabled)
-                    ShowDebug();
-                else
-                    HideDebug();
+                EnabledText = !EnabledText;
             }
-
-            if (DebugEnabled && textObjects.Count >= NUM_TEXT_LINES && Core.Logic.Penitent != null)
+            if (Input.GetKeyDown(KeyCode.Keypad8))
             {
-                textObjects[0].text = $"Scene: " + Core.LevelManager.currentLevel.LevelName;
-
-                Vector2 position = Core.Logic.Penitent.transform.position;
-                textObjects[1].text = $"Position: ({RoundToOne(position.x)}, {RoundToOne(position.y)})";
-
-                Vector2 health = new Vector2(Core.Logic.Penitent.Stats.Life.Current, Core.Logic.Penitent.Stats.Life.CurrentMax);
-                textObjects[2].text = $"HP: {RoundToOne(health.x)}/{RoundToOne(health.y)}";
-
-                Vector2 fervour = new Vector2(Core.Logic.Penitent.Stats.Fervour.Current, Core.Logic.Penitent.Stats.Fervour.CurrentMax);
-                textObjects[3].text = $"FP: {RoundToOne(fervour.x)}/{RoundToOne(fervour.y)}";
+                EnabledHitboxes = !EnabledHitboxes;
+            }
+            if (Input.GetKeyDown(KeyCode.Keypad9))
+            {
+                EnabledFreeCam = !EnabledFreeCam;
             }
         }
 
         protected override void LateUpdate()
         {
-            if (Camera.main == null) return;
+            UpdateDebugText();
+            UpdateFreeCam();
+        }
 
-            if (DebugEnabled)
+        protected override void LevelLoaded(string oldLevel, string newLevel)
+        {
+            if (EnabledText)
+                ShowDebugText();
+            if (EnabledHitboxes)
+                ShowHitboxes();
+        }
+
+        protected override void LevelUnloaded(string oldLevel, string newLevel)
+        {
+            HideDebugText();
+            HideHitboxes();
+            EnabledFreeCam = false;
+        }
+
+        #region Debug Text
+
+        private bool _enabledText = false;
+        public bool EnabledText
+        {
+            get { return _enabledText; }
+            set
             {
-                if (Input.GetKey(KeyCode.LeftArrow))
+                _enabledText = value;
+                if (value)
                 {
-                    cameraPosition += Vector3.left * CAMERA_SPEED;
+                    ShowDebugText();
                 }
-                if (Input.GetKey(KeyCode.RightArrow))
+                else
                 {
-                    cameraPosition += Vector3.right * CAMERA_SPEED;
+                    HideDebugText();
                 }
-                if (Input.GetKey(KeyCode.DownArrow))
-                {
-                    cameraPosition += Vector3.down * CAMERA_SPEED;
-                }
-                if (Input.GetKey(KeyCode.UpArrow))
-                {
-                    cameraPosition += Vector3.up * CAMERA_SPEED;
-                }
-                Camera.main.transform.position = cameraPosition;
-            }
-            else
-            {
-                cameraPosition = Camera.main.transform.position;
             }
         }
 
-        protected override void LevelLoaded(string oldLevel, string newLevel) { ShowDebug(); }
-        protected override void LevelUnloaded(string oldLevel, string newLevel) { HideDebug(); }
-
-        private string RoundToOne(float value)
-        {
-            return value.ToString("F1");
-        }
-
-        private void ShowDebug()
-        {
-            if (DebugEnabled)
-            {
-                AddHitboxes();
-                EnableText();
-            }
-        }
-
-        private void HideDebug()
-        {
-            RemoveHitboxes();
-            DisableText();
-        }
-
-        private void AddHitboxes()
-        {
-            GameObject baseHitbox = CreateBaseHitbox();
-            sceneHitboxes.Clear();
-
-            foreach (BoxCollider2D collider in Object.FindObjectsOfType<BoxCollider2D>())
-            {
-                if (collider.name.StartsWith(GEOMETRY_NAME)) continue;
-
-                GameObject hitbox = Object.Instantiate(baseHitbox, collider.transform);
-                hitbox.transform.localPosition = Vector3.zero;
-
-                Transform side = hitbox.transform.GetChild(0);
-                side.localPosition = new Vector3(collider.offset.x, collider.size.y / 2 + collider.offset.y, 0);
-                side.localScale = new Vector3(collider.size.x, SCALE_AMOUNT / collider.transform.localScale.y, 0);
-
-                side = hitbox.transform.GetChild(1);
-                side.localPosition = new Vector3(-collider.size.x / 2 + collider.offset.x, collider.offset.y, 0);
-                side.localScale = new Vector3(SCALE_AMOUNT / collider.transform.localScale.x, collider.size.y, 0);
-
-                side = hitbox.transform.GetChild(2);
-                side.localPosition = new Vector3(collider.size.x / 2 + collider.offset.x, collider.offset.y, 0);
-                side.localScale = new Vector3(SCALE_AMOUNT / collider.transform.localScale.x, collider.size.y, 0);
-
-                side = hitbox.transform.GetChild(3);
-                side.localPosition = new Vector3(collider.offset.x, -collider.size.y / 2 + collider.offset.y, 0);
-                side.localScale = new Vector3(collider.size.x, SCALE_AMOUNT / collider.transform.localScale.y, 0);
-
-                sceneHitboxes.Add(hitbox);
-            }
-            Object.Destroy(baseHitbox);
-
-            Log($"Adding outlines to {sceneHitboxes.Count} hitboxes");
-        }
-
-        private void RemoveHitboxes()
-        {
-            for (int i = 0; i < sceneHitboxes.Count; i++)
-            {
-                if (sceneHitboxes[i] != null)
-                    Object.Destroy(sceneHitboxes[i]);
-            }
-            sceneHitboxes.Clear();
-        }
-
-        private GameObject CreateBaseHitbox()
-        {
-            GameObject baseHitbox = new GameObject("Hitbox");
-            GameObject side = new GameObject("TOP");
-            side.AddComponent<SpriteRenderer>().sprite = hitboxImage;
-            side.transform.parent = baseHitbox.transform;
-            side = new GameObject("LEFT");
-            side.AddComponent<SpriteRenderer>().sprite = hitboxImage;
-            side.transform.parent = baseHitbox.transform;
-            side = new GameObject("RIGHT");
-            side.AddComponent<SpriteRenderer>().sprite = hitboxImage;
-            side.transform.parent = baseHitbox.transform;
-            side = new GameObject("BOTTOM");
-            side.AddComponent<SpriteRenderer>().sprite = hitboxImage;
-            side.transform.parent = baseHitbox.transform;
-            return baseHitbox;
-        }
-
-        private void EnableText()
+        private void ShowDebugText()
         {
             if (textObjects.Count == 0)
             {
@@ -184,11 +96,33 @@ namespace DebugMod
             }
         }
 
-        private void DisableText()
+        private void HideDebugText()
         {
             for (int i = 0; i < textObjects.Count; i++)
             {
                 textObjects[i].gameObject.SetActive(false);
+            }
+        }
+
+        private void UpdateDebugText()
+        {
+            if (EnabledText && textObjects.Count >= NUM_TEXT_LINES && Core.Logic.Penitent != null)
+            {
+                textObjects[0].text = $"Scene: " + Core.LevelManager.currentLevel.LevelName;
+
+                Vector2 position = Core.Logic.Penitent.transform.position;
+                textObjects[1].text = $"Position: ({RoundToOne(position.x)}, {RoundToOne(position.y)})";
+
+                Vector2 health = new Vector2(Core.Logic.Penitent.Stats.Life.Current, Core.Logic.Penitent.Stats.Life.CurrentMax);
+                textObjects[2].text = $"HP: {RoundToOne(health.x)}/{RoundToOne(health.y)}";
+
+                Vector2 fervour = new Vector2(Core.Logic.Penitent.Stats.Fervour.Current, Core.Logic.Penitent.Stats.Fervour.CurrentMax);
+                textObjects[3].text = $"FP: {RoundToOne(fervour.x)}/{RoundToOne(fervour.y)}";
+            }
+
+            string RoundToOne(float value)
+            {
+                return value.ToString("F1");
             }
         }
 
@@ -228,5 +162,125 @@ namespace DebugMod
                 textObjects.Add(text);
             }
         }
+
+        #endregion Debug Text
+
+        #region Hitboxes
+
+        private bool _enabledHitboxes = false;
+        public bool EnabledHitboxes
+        {
+            get { return _enabledHitboxes; }
+            set
+            {
+                _enabledHitboxes = value;
+                if (value)
+                {
+                    ShowHitboxes();
+                }
+                else
+                {
+                    HideHitboxes();
+                }
+            }
+        }
+
+        private void ShowHitboxes()
+        {
+            GameObject baseHitbox = CreateBaseHitbox();
+            sceneHitboxes.Clear();
+
+            foreach (BoxCollider2D collider in Object.FindObjectsOfType<BoxCollider2D>())
+            {
+                if (collider.name.StartsWith(GEOMETRY_NAME)) continue;
+
+                GameObject hitbox = Object.Instantiate(baseHitbox, collider.transform);
+                hitbox.transform.localPosition = Vector3.zero;
+
+                Transform side = hitbox.transform.GetChild(0);
+                side.localPosition = new Vector3(collider.offset.x, collider.size.y / 2 + collider.offset.y, 0);
+                side.localScale = new Vector3(collider.size.x, SCALE_AMOUNT / collider.transform.localScale.y, 0);
+
+                side = hitbox.transform.GetChild(1);
+                side.localPosition = new Vector3(-collider.size.x / 2 + collider.offset.x, collider.offset.y, 0);
+                side.localScale = new Vector3(SCALE_AMOUNT / collider.transform.localScale.x, collider.size.y, 0);
+
+                side = hitbox.transform.GetChild(2);
+                side.localPosition = new Vector3(collider.size.x / 2 + collider.offset.x, collider.offset.y, 0);
+                side.localScale = new Vector3(SCALE_AMOUNT / collider.transform.localScale.x, collider.size.y, 0);
+
+                side = hitbox.transform.GetChild(3);
+                side.localPosition = new Vector3(collider.offset.x, -collider.size.y / 2 + collider.offset.y, 0);
+                side.localScale = new Vector3(collider.size.x, SCALE_AMOUNT / collider.transform.localScale.y, 0);
+
+                sceneHitboxes.Add(hitbox);
+            }
+
+            Object.Destroy(baseHitbox);
+            Log($"Adding outlines to {sceneHitboxes.Count} hitboxes");
+        }
+
+        private void HideHitboxes()
+        {
+            for (int i = 0; i < sceneHitboxes.Count; i++)
+            {
+                if (sceneHitboxes[i] != null)
+                    Object.Destroy(sceneHitboxes[i]);
+            }
+            sceneHitboxes.Clear();
+        }
+
+        private GameObject CreateBaseHitbox()
+        {
+            GameObject baseHitbox = new GameObject("Hitbox");
+            GameObject side = new GameObject("TOP");
+            side.AddComponent<SpriteRenderer>().sprite = hitboxImage;
+            side.transform.parent = baseHitbox.transform;
+            side = new GameObject("LEFT");
+            side.AddComponent<SpriteRenderer>().sprite = hitboxImage;
+            side.transform.parent = baseHitbox.transform;
+            side = new GameObject("RIGHT");
+            side.AddComponent<SpriteRenderer>().sprite = hitboxImage;
+            side.transform.parent = baseHitbox.transform;
+            side = new GameObject("BOTTOM");
+            side.AddComponent<SpriteRenderer>().sprite = hitboxImage;
+            side.transform.parent = baseHitbox.transform;
+            return baseHitbox;
+        }
+
+        #endregion Hitboxes
+
+        #region Free Cam
+
+        private bool _enabledFreeCam = false;
+        public bool EnabledFreeCam
+        {
+            get { return _enabledFreeCam; }
+            set { _enabledFreeCam = value; }
+        }
+
+        private void UpdateFreeCam()
+        {
+            if (Camera.main == null) return;
+
+            if (EnabledFreeCam)
+            {
+                if (Input.GetKey(KeyCode.LeftArrow))
+                    cameraPosition += Vector3.left * CAMERA_SPEED;
+                if (Input.GetKey(KeyCode.RightArrow))
+                    cameraPosition += Vector3.right * CAMERA_SPEED;
+                if (Input.GetKey(KeyCode.DownArrow))
+                    cameraPosition += Vector3.down * CAMERA_SPEED;
+                if (Input.GetKey(KeyCode.UpArrow))
+                    cameraPosition += Vector3.up * CAMERA_SPEED;
+                Camera.main.transform.position = cameraPosition;
+            }
+            else
+            {
+                cameraPosition = Camera.main.transform.position;
+            }
+        }
+
+        #endregion Free Cam
     }
 }
