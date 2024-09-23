7c57c56bd14f9f9d1734bea1a57f9f76cf6b935b
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-09-23 19:52:33 -0400

    core: upgrade packages

--- Blasphemous.DebugMod/Main.cs
+++ Blasphemous.DebugMod/Main.cs
@@ -3,8 +3,8 @@
 namespace Blasphemous.DebugMod;
 
 [BepInPlugin(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_VERSION)]
-[BepInDependency("Blasphemous.ModdingAPI", "2.1.0")]
-[BepInDependency("Blasphemous.Framework.UI", "0.1.0")]
+[BepInDependency("Blasphemous.ModdingAPI", "2.4.1")]
+[BepInDependency("Blasphemous.Framework.UI", "0.1.2")]
 internal class Main : BaseUnityPlugin
 {
     public static Debugger Debugger { get; private set; }
