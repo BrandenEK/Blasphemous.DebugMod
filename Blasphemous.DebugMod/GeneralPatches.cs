be53ecca6b60ff079b79676e3405edc5c1c49b1a
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-02-03 12:14:38 -0500

    core: fix patch

--- Blasphemous.DebugMod/GeneralPatches.cs
+++ Blasphemous.DebugMod/GeneralPatches.cs
@@ -4,7 +4,7 @@ using HarmonyLib;
 namespace Blasphemous.DebugMod;
 
 // Always allow cursor visibility
-[HarmonyPatch(typeof(DebugInformation), "Update")]
+[HarmonyPatch(typeof(DebugInformation), nameof(DebugInformation.Update))]
 class DebugInformation_Update_Patch
 {
     public static bool Prefix() => false;
